2f938f1667c1e7050f607642bb6d830f49f12adf
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-07-09 16:05:57 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <Router
+        ><App />
+    </Router>
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
 
